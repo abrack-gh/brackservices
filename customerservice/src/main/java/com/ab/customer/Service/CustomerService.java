9cdf55e3374a27af8a9799c7c0ca5ee21109d1c8
Alexander Brackenridge <alexbrackenridge@alexanders-air.home>
2023-08-02 15:44:14 +0100

    Progress so far

--- customerservice/src/main/java/com/ab/customer/Service/CustomerService.java
+++ customerservice/src/main/java/com/ab/customer/Service/CustomerService.java
@@ -1,16 +1,19 @@
 package com.ab.customer.Service;
 
 import com.ab.customer.Model.Customer;
+import com.ab.customer.Repository.CustomerRepository;
 import com.ab.customer.Request.CustomerRegistrationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRequest.firstName())
                 .lastName(customerRequest.lastName())
                 .email(customerRequest.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }

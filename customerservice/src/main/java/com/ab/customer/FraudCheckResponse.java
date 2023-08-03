29f58ae3f3c2fb6ac654b6e5273e9738581a980c
Alexander Brackenridge <alexbrackenridge@alexanders-air.home>
2023-08-03 13:19:51 +0100

    Fixed Eureka server incompatible versions

---
+++ customerservice/src/main/java/com/ab/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.ab.customer;
+
+public record FraudCheckResponse(Boolean isFraudulent) {
+}
--- customerservice/src/main/java/com/ab/customer/Service/CustomerService.java
+++ customerservice/src/main/java/com/ab/customer/Service/CustomerService.java
@@ -1,16 +1,21 @@
 package com.ab.customer.Service;
 
+import com.ab.customer.FraudCheckResponse;
 import com.ab.customer.Model.Customer;
 import com.ab.customer.Repository.CustomerRepository;
 import com.ab.customer.Request.CustomerRegistrationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -19,6 +24,20 @@ public class CustomerService {
                 .email(customerRequest.email())
                 .build();
 
-        customerRepository.save(customer);
+
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+
+        );
+
+        assert fraudCheckResponse != null;
+        if(fraudCheckResponse.isFraudulent()){
+            throw new IllegalStateException("Fraud detected");
+        }
+
     }
 }
